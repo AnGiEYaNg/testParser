9e0bef67a2ee6befc51995b6cd8a03fd7d6f5363
Angie <angi3yang@gmail.com>
2016-07-13 17:47:28 -0400

    add in css and angular

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+//serves up angular js files and htmls
+app.use(express.static(__dirname+'/public'));
+
+//serves up css files
+app.use(express.static(__dirname+'/assets'));
+
+//homepage
 app.get('/', function(req, res){
 	res.sendFile(__dirname+"/index.html");
 })
