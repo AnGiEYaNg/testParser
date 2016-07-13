d4529fb5d5ca1f9b5aa8add552fe10c56806b362
Angie <angi3yang@gmail.com>
2016-07-13 19:43:35 -0400

    parse in fb but error on watch

--- public/app.js
+++ public/app.js
@@ -1,2 +1,52 @@
 'use strict';
 var app = angular.module("TestParser", []);
+
+app.run(['$rootScope', '$window', 'sAuth',
+  function($rootScope, $window, sAuth) {
+
+  $rootScope.user = {};
+
+  $window.fbAsyncInit = function() {
+    // Executed when the SDK is loaded
+
+    FB.init({
+
+      appId: '1778335662380031',
+      /*
+	     Adding a Channel File improves the performance
+	     of the javascript SDK, by addressing issues
+	     with cross-domain communication in certain browsers.
+	   */
+
+      channelUrl: 'channel.html',
+
+      /* Parse XFBML */
+
+      xfbml: true
+    });
+
+    sAuth.watchAuthenticationStatusChange();
+
+  };
+
+  (function(d){
+    // load the Facebook javascript SDK
+
+    var js,
+    id = 'facebook-jssdk',
+    ref = d.getElementsByTagName('script')[0];
+
+    if (d.getElementById(id)) {
+      return;
+    }
+
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+
+    ref.parentNode.insertBefore(js, ref);
+
+  }(document));
+
+}]);
